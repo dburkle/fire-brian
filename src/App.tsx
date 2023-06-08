155b7d23a2cd876169d4fa64b00d1f132d955807
Daniel Burkle <dnburkle@gmail.com>
2023-06-08 09:54:37 -0500

    Trevor no

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className='App__header'>Something something Brian Ferentz</div>
-      <div>
-        <div>Brandon is an r-tard</div>
-        <div>Trevor is a monkey brain</div>
-        <div>Guy can eat a weiner</div>
-        <div>Feels? Feels can go fuck himself</div>
-      </div>
-      <div>Stats go here</div>
-      <div>
-        <div>Boo I miss u bby</div>
-      </div>
+    <div className="App"> 
+        <div className='App__header'>Trevor no</div>
+        <div>Guy whatever happened to getting stats?</div>
     </div>
   );
 }
